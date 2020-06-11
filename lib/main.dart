e784d412cfd8feddf369a3e02678ecacd7e867d5
ilovecmyk <mario.be@online.de>
2020-06-11 18:10:59 +0200

    bla

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain(); // Creating the new object quizBrain
 
 void main() => runApp(Quizzler());
 
@@ -26,11 +29,18 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> quizQuestions = [
+/*   List<String> quizQuestions = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
   ];
+  List<bool> answers = [false, true, true]; */
+
+
+
+
+  
+  
 
   int questionNumber = 0;
 
@@ -46,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizQuestions[questionNumber],
+                quizBrain.questionAndAnswers[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -70,6 +80,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questionAndAnswers[questionNumber].questionAnswer;
+
+                if (correctAnswer == true) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+
                 setState(() {
                   //The user picked true.
                   //questionNumber = questionNumber + 1;
@@ -98,9 +116,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.questionAndAnswers[questionNumber].questionAnswer;
+
+                if (correctAnswer == false) {
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
                 setState(() {
                   //The user picked false.
-                  questionNumber = questionNumber + 1;
+                  questionNumber++;
                   scoreKeeper.add(
                     Icon(
                       Icons.close,
---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+class Question {
+  // classes start by convention with capital letter!
+
+  String questionText; //variables
+  bool questionAnswer;
+
+  Question(String q, bool a) {
+    //This ithe constructor
+    questionText = q;
+    questionAnswer = a;
+  }
+}
+
